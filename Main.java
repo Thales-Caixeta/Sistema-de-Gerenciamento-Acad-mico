cf31ce9957cf9a4cd4a3cde8913ea20190993727
Thales Moura Machado Caixeta <thalesmachado444@gmail.com>
2024-11-28 23:31:00 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        // Inicia a tela do menu principal
+        SwingUtilities.invokeLater(() -> new Menu());
+    }
+}
